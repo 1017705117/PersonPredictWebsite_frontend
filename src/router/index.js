d7a084cc0b3356952eb5d281acb150a856de2394
1017705117 <48436640+1017705117@users.noreply.github.com>
2021-02-06 17:15:16 +0800

    fix:新增build和config

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Main from '../components/Main.vue'
+import Main from '@/components/Main.vue'
+import NotFind from '@/components/NotFind.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'Main',
       component: Main
+    },
+    {
+      path: '/404',
+      component: NotFind
     }
   ]
 })
